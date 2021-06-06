ebcc49a6c3c13d5282a56ca430b785235dafa52d
Aluizio Amaro de Sousa <aluizio.amaro@gmail.com>
2021-06-06 13:31:32 -0300

    Criacao do projeto

---
+++ AppConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
